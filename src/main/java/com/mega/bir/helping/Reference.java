6b00d4dbd58c4505d2067b7e617b2d177c21b577
Megabitus <megabitus98@gmail.com>
2014-08-11 11:48:51 +0300

    Added References file!

---
+++ src/main/java/com/mega/bir/helping/Reference.java
@@ -0,0 +1,14 @@
+package com.mega.bir.helping;
+
+/**
+ * Created by Megabitus on 8/11/2014 and hour 11.
+ */
+
+public class Reference {
+        public static final String MOD_ID = "BiR";
+    public static final String MOD_NAME = "Blocks & Items Revolution";
+    public static final String VERSION = "1.7.10-1.0";
+    public static final String SERVER_PROXY_CLASS = "com.mega.bir.proxy.ServerProxy";
+    public static final String CLIENT_PROXY_CLASS = "com.mega.bir.proxy.ClientProxy";
+    public static final String GUI_FACTORY_CLASS = "com.mega.bir.client.gui.GuiFactory";
+}
