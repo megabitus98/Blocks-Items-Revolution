2f4210fde4fb20efa36b2435ac422f23ae102117
Megabitus <megabitus98@gmail.com>
2014-08-11 19:46:29 +0300

    Added a new block

--- src/main/java/com/mega/bir/block/BlocksManager.java
+++ src/main/java/com/mega/bir/block/BlocksManager.java
@@ -1,6 +1,7 @@
 package com.mega.bir.block;
 
 import com.mega.bir.block.blocks.BlockInterChest;
+import com.mega.bir.block.blocks.BlockTelotriteOre;
 import com.mega.bir.helping.Names;
 import com.mega.bir.helping.Reference;
 
@@ -13,9 +14,12 @@ import cpw.mods.fml.common.registry.GameRegistry;
 @GameRegistry.ObjectHolder(Reference.MOD_ID)
 public class BlocksManager {
     public static final BlockBir interChest = new BlockInterChest();
+    public static final BlockBir telotriteOre = new BlockTelotriteOre();
     
     public static void register()
     {
         GameRegistry.registerBlock(interChest, Names.Block_Inter_Chest);
+        GameRegistry.registerBlock(telotriteOre, Names.Block_Telotrite_Ore);
+
     }
 }
---
+++ src/main/java/com/mega/bir/block/blocks/BlockTelotriteOre.java
@@ -0,0 +1,16 @@
+package com.mega.bir.block.blocks;
+
+import com.mega.bir.block.BlockBir;
+import com.mega.bir.helping.Names;
+
+/**
+ * Created by Megabitus on 8/11/2014 and hour 19.
+ */
+
+public class BlockTelotriteOre extends BlockBir {
+    public BlockTelotriteOre(){
+        super();
+        this.setBlockName(Names.Block_Telotrite_Ore);
+        this.setBlockTextureName(Names.Block_Telotrite_Ore);
+    }
+}
