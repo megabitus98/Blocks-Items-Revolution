3c7fd6c53fb9d3be99b8c1678bbcc1f1a8b05d06
Megabitus <megabitus98@gmail.com>
2014-08-11 11:47:43 +0300

    Added Proxys

---
+++ src/main/java/com/mega/bir/proxy/CommonProxy.java
@@ -0,0 +1,10 @@
+package com.mega.bir.proxy;
+
+/**
+ * Created by Megabitus on 8/10/2014.
+ */
+
+public abstract class CommonProxy implements IProxy
+{
+
+}
---
+++ src/main/java/com/mega/bir/proxy/IProxy.java
@@ -0,0 +1,9 @@
+package com.mega.bir.proxy;
+
+/**
+ * Created by Megabitus on 8/10/2014.
+ */
+public interface IProxy
+{
+
+}
